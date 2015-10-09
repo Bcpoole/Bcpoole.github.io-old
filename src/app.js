f4e71fc485c59230e210efb0f4ad983825931a07
Bcpoole <brandoncpoole@gmail.com>
2015-10-08 21:57:38 -0500

    Updated nav-bar

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Brandon Poole';
     config.map([
-      { route: ['','welcome'],  name: 'welcome',      moduleId: 'welcome',      nav: true, title:'Home' }
+      { route: ['','welcome'],  name: 'welcome',      moduleId: 'welcome',      nav: false, title:'Home' }
     ]);
 
     this.router = router;
