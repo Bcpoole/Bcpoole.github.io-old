7d8b904c4f86132c0fed81d13f1673d79798d8a5
Bcpoole <brandoncpoole@gmail.com>
2015-10-10 14:00:30 -0500

    Added LinkedIn account link on welcome page

--- dist/welcome.js
+++ dist/welcome.js
@@ -13,10 +13,11 @@ System.register([], function (_export) {
 
         this.heading = 'Under Construction!';
         this.githubLink = 'https://github.com/Bcpoole';
+        this.linkedinLink = 'https://www.linkedin.com/pub/brandon-poole/61/29a/b75';
       };
 
       _export('Welcome', Welcome);
     }
   };
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlbGNvbWUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O01BQWEsT0FBTzs7Ozs7OztBQUFQLGFBQU8sWUFBUCxPQUFPOzhCQUFQLE9BQU87O2FBQ2xCLE9BQU8sR0FBRyxxQkFBcUI7YUFDL0IsVUFBVSxHQUFHLDRCQUE0QiIsImZpbGUiOiJ3ZWxjb21lLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIFdlbGNvbWUge1xuICBoZWFkaW5nID0gJ1VuZGVyIENvbnN0cnVjdGlvbiEnO1xuICBnaXRodWJMaW5rID0gJ2h0dHBzOi8vZ2l0aHViLmNvbS9CY3Bvb2xlJztcbn1cbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlbGNvbWUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O01BQWEsT0FBTzs7Ozs7OztBQUFQLGFBQU8sWUFBUCxPQUFPOzhCQUFQLE9BQU87O2FBQ2xCLE9BQU8sR0FBRyxxQkFBcUI7YUFDL0IsVUFBVSxHQUFHLDRCQUE0QjthQUN6QyxZQUFZLEdBQUcsdURBQXVEIiwiZmlsZSI6IndlbGNvbWUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY2xhc3MgV2VsY29tZSB7XG4gIGhlYWRpbmcgPSAnVW5kZXIgQ29uc3RydWN0aW9uISc7XG4gIGdpdGh1YkxpbmsgPSAnaHR0cHM6Ly9naXRodWIuY29tL0JjcG9vbGUnO1xuICBsaW5rZWRpbkxpbmsgPSAnaHR0cHM6Ly93d3cubGlua2VkaW4uY29tL3B1Yi9icmFuZG9uLXBvb2xlLzYxLzI5YS9iNzUnO1xufVxuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
--- src/welcome.js
+++ src/welcome.js
@@ -1,4 +1,5 @@
 export class Welcome {
   heading = 'Under Construction!';
   githubLink = 'https://github.com/Bcpoole';
+  linkedinLink = 'https://www.linkedin.com/pub/brandon-poole/61/29a/b75';
 }
