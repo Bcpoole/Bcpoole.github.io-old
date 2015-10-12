2343d7b63801acd3cec980a1d9a8b1ed6edb772d
Bcpoole <brandoncpoole@gmail.com>
2015-10-11 21:27:06 -0500

    Removed "Under Construction". Formatting. Updated About Me info (additional, typos, hyperlinks, etc). Removed dead tag.

--- dist/welcome.js
+++ dist/welcome.js
@@ -11,7 +11,6 @@ System.register([], function (_export) {
       Welcome = function Welcome() {
         _classCallCheck(this, Welcome);
 
-        this.heading = 'Under Construction!';
         this.githubLink = 'https://github.com/Bcpoole';
         this.linkedinLink = 'https://www.linkedin.com/pub/brandon-poole/61/29a/b75';
       };
@@ -20,4 +19,4 @@ System.register([], function (_export) {
     }
   };
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlbGNvbWUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O01BQWEsT0FBTzs7Ozs7OztBQUFQLGFBQU8sWUFBUCxPQUFPOzhCQUFQLE9BQU87O2FBQ2xCLE9BQU8sR0FBRyxxQkFBcUI7YUFDL0IsVUFBVSxHQUFHLDRCQUE0QjthQUN6QyxZQUFZLEdBQUcsdURBQXVEIiwiZmlsZSI6IndlbGNvbWUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY2xhc3MgV2VsY29tZSB7XG4gIGhlYWRpbmcgPSAnVW5kZXIgQ29uc3RydWN0aW9uISc7XG4gIGdpdGh1YkxpbmsgPSAnaHR0cHM6Ly9naXRodWIuY29tL0JjcG9vbGUnO1xuICBsaW5rZWRpbkxpbmsgPSAnaHR0cHM6Ly93d3cubGlua2VkaW4uY29tL3B1Yi9icmFuZG9uLXBvb2xlLzYxLzI5YS9iNzUnO1xufVxuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlbGNvbWUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O01BQWEsT0FBTzs7Ozs7OztBQUFQLGFBQU8sWUFBUCxPQUFPOzhCQUFQLE9BQU87O2FBQ2xCLFVBQVUsR0FBRyw0QkFBNEI7YUFDekMsWUFBWSxHQUFHLHVEQUF1RCIsImZpbGUiOiJ3ZWxjb21lLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIFdlbGNvbWUge1xuICBnaXRodWJMaW5rID0gJ2h0dHBzOi8vZ2l0aHViLmNvbS9CY3Bvb2xlJztcbiAgbGlua2VkaW5MaW5rID0gJ2h0dHBzOi8vd3d3LmxpbmtlZGluLmNvbS9wdWIvYnJhbmRvbi1wb29sZS82MS8yOWEvYjc1Jztcbn1cbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
