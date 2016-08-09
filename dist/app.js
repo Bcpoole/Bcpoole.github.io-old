831159cc25b8690ed69c4ff869faff2926cf5cde
Bcpoole <brandoncpoole@gmail.com>
2016-08-08 23:56:17 -0400

    Added Resume tab. Updated build. Added styling for iframe

--- dist/app.js
+++ dist/app.js
@@ -21,7 +21,7 @@ System.register([], function (_export, _context) {
 
         App.prototype.configureRouter = function configureRouter(config, router) {
           config.title = 'Brandon Poole';
-          config.map([{ route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: false, title: 'Welcome' }]);
+          config.map([{ route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: false, title: 'Welcome' }, { route: 'resume', name: 'resume', moduleId: 'resume', nav: true, title: 'Resume' }]);
 
           this.router = router;
         };
--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Brandon Poole';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: false, title: 'Welcome' }
+      { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: false, title: 'Welcome' },
+      { route: 'resume', name: 'resume', moduleId: 'resume', nav: true, title: 'Resume' }      
     ]);
 
     this.router = router;
